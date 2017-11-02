d800f29d272485c39c90b78c3d956475eb8b5fe4
Harry Caufield <j.harry.caufield@gmail.com>
2017-11-01 22:11:28 -0700

    Initial commit

---
+++ birchcanoe.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#birchcanoe.py
+'''
+Generates novels and their accompanying audio based on 
+phonetically-optimal English sentences. #NaNoGenMo2017
+
+Uses the Harvard Sentences - see
+http://ieeexplore.ieee.org/document/1162058/
+'''
+
+import sys
+
+#Contants and Setup
+
+#Methods
+
+#Main
+def main():
+	pass
+
+if __name__ == "__main__":
+	sys.exit(main())
